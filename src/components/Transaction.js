f04fd65a8796fad363591bc8a3f135719833f87b
lonnalu <lonnalu@gmail.com>
2021-11-12 10:03:05 -0800

    Adds more styling to transaction history box

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,13 +2,15 @@ const Transaction = ({ transaction }) => {
   const dateString = new Date(transaction.date).toLocaleDateString();
 
   return (
-    <div>
-      {dateString} 
-      {transaction.text} 
-      {transaction.amount > 0 
+    <div className="transaction">
+      <p>{dateString} </p>
+      <p>{transaction.text} </p>
+      <p className={"transaction-amount " + (transaction.amount > 0 ? "" : "expense-text") }>
+        {transaction.amount > 0 
         ? `$${(transaction.amount).toFixed(2)}` 
         : `-$${Math.abs((transaction.amount)).toFixed(2)}`
-      }
+        }
+      </p>
     </div>
   )
 }
--- src/components/TransactionContainer.js
+++ src/components/TransactionContainer.js
@@ -6,6 +6,8 @@ const TransactionContainer = () => {
   const { transactions } = useContext(GlobalContext);
   return (
     <div className="transaction-container">
+      <h2>Transaction History</h2>
+      <hr />
     {transactions.map((transaction,idx) => <Transaction key={idx} transaction={transaction} />)}
     </div>
   )
